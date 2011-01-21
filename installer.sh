53949a9aa4532072661d6c80b4fa436c5e3b6a49
Eitan Adler <lists@eitanadler.com>
2011-01-21 12:04:29 -0500

    add the X Compose file

--- installer.sh
+++ installer.sh
@@ -5,3 +5,5 @@ self=$(realpath ~/.conf);
 [ -e $HOME/.zshrc ] || ln -s $self/zsh/zshrc $HOME/.zshrc
 [ -e $HOME/.nanorc ] || ln -s $self/nano/USE_RC $HOME/.nanorc
 [ -e $HOME/.mynano ] || ln -s $self/nano/ $HOME/.mynano
+[ -e $HOME/.XCompose ] || ln -s $self/X/XCompose $HOME/.XCompose
+
