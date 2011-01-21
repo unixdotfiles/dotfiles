d21e9943722855f3575fae052a9e107e3759ee72
Eitan Adler <lists@eitanadler.com>
2011-01-21 12:07:25 -0500

    add mercurial configuration file

--- installer.sh
+++ installer.sh
@@ -6,4 +6,5 @@ self=$(realpath ~/.conf);
 [ -e $HOME/.nanorc ] || ln -s $self/nano/USE_RC $HOME/.nanorc
 [ -e $HOME/.mynano ] || ln -s $self/nano/ $HOME/.mynano
 [ -e $HOME/.XCompose ] || ln -s $self/X/XCompose $HOME/.XCompose
+[ -e $HOME/.hgrc ] || ln -s $self/mercurial/hgrc $HOME/.hgrc
 
