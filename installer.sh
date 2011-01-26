fe3d0e2ad41910bf89f5a3079bc876ce507f3569
eitan <devnull@localhost>
2011-01-26 07:25:46 -0500

    make some simple functions for the installer to reduce duplicate work

--- installer.sh
+++ installer.sh
@@ -1,11 +1,20 @@
 #! /bin/sh -x
 self=$(realpath ~/.conf);
 
+#Test ln. only link if the second argument doesn't exist
+tln () {
+	[ -e "$2" ] || ln -s "$1" "$2"
+}
 
-[ -e $HOME/.zshrc ] || ln -s $self/zsh/zshrc $HOME/.zshrc
-[ -e $HOME/.nanorc ] || ln -s $self/nano/USE_RC $HOME/.nanorc
-[ -e $HOME/.mynano ] || ln -s $self/nano/ $HOME/.mynano
-[ -e $HOME/.XCompose ] || ln -s $self/X/XCompose $HOME/.XCompose
-[ -e $HOME/.hgrc ] || ln -s $self/mercurial/hgrc $HOME/.hgrc
-[ -e $HOME/.mplayer/config ] || ln -s $self/mplayer/config $HOME/.mplayer/config
+#Home test ln. Like tln but assumes everything is relative to $HOME && $self
+htln () {
+	tln "$self/$1" "$HOME/$2"
+}
+
+htln zsh/zshrc .zshrc
+htln nano/USE_RC .nanorc
+htln nano .mynano
+htln X/XCompose .XCompose
+htln mercurial/hgrc .hgrc
+htln mplayer/config .mplayer/config
 
