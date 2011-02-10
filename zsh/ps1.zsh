5f8968349d6dedfc9ab55bdf9beae1145ef0034d
Eitan Adler <lists@eitanadler.com>
2011-02-10 16:57:17 -0500

    get a load of data from https://code.google.com/p/asmeniniai/

--- zsh/ps1.zsh
+++ zsh/ps1.zsh
@@ -1,4 +1,24 @@
 autoload -Uz vcs_info
+zstyle ':vcs_info:hg*:' use-simple true 
+zstyle ':vcs_info:*' enable svn git hg cvs
+# [ formats ] 
+#zstyle ':vcs_info:*' formats "%b%c%u"
+#zstyle ':vcs_info:*' disable cdv darcs mtn p4 svk tla git-p4 git-svn bzr
+######edit    zstyle ':vcs_info:*' actionformats "%b%c%u|%a"
+#zstyle ':vcs_info:*' branchformat "%b:%r"
+zstyle ':vcs_info:*' check-for-changes false
+zstyle ':vcs_info:hg*:' get-revision false
+zstyle ':vcs_info:git*:' get-revision false
+
+# [ stagedstr ]
+# This string will be used in the %c escape if there are staged changes in the repository. 
+zstyle ':vcs_info:*' stagedstr "+"
+# [ unstagedstr ]
+# This string will be used in the %u escape if there are unstaged changes in the repository. 
+zstyle ':vcs_info:*' unstagedstr "!"
+
+zstyle ':vcs_info:p4*:' use-server false
+
 __vcs_dir() {
 	vcs_info
 	echo $vcs_info_msg_0_;
