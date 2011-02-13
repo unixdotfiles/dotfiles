8badf382ffeac86bb79642bc490bb4f9993780a7
eitan <devnull@localhost>
2011-02-13 15:27:22 -0500

    add space to fix bashrc

--- bash/bash_profile_primary.bash
+++ bash/bash_profile_primary.bash
@@ -1,7 +1,7 @@
 #used for interactive shells
 if [ -t 0 ]
 then
-	[-e /usr/bin/zsh ] && exec /usr/bin/zsh
+	[ -e /usr/bin/zsh ] && exec /usr/bin/zsh
 	[ -e /bin/zsh ] && exec /bin/zsh
 	[ -e /usr/local/bin/zsh ] && exec /usr/local/bin/zsh
 fi
