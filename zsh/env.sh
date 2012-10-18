678fb9857b43959ad7401601c46f667ca5c68763
Eitan Adler <lists@eitanadler.com>
2012-10-18 19:15:48 -0400

    finally figured how to use the LESS version

--- zsh/env.sh
+++ zsh/env.sh
@@ -1,7 +1,7 @@
 export BLOCKSIZE=K;
 export EDITOR=vim;
 export PAGER=less;
-#export LESS="M"
+export LESS="R F M X"
 
 export PATH="/usr/local/libexec/ccache:$PATH:$HOME/bin";
 export CCACHE_PATH=/usr/local/bin:/usr/bin
