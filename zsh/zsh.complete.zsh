f746d1ba062dfd7932cc2c92fa10664189aab4fb
Eitan Adler <lists@eitanadler.com>
2012-10-26 16:40:22 -0400

    remove irelevent option

--- zsh/zsh.complete.zsh
+++ zsh/zsh.complete.zsh
@@ -10,7 +10,6 @@
 # Global completion settings {{{
 
 	zstyle :compinstall filename ~/.zshrc
-	unsetopt flowcontrol
 	setopt auto_menu         # show completion menu on succesive tab press
 	setopt complete_in_word
 	setopt always_to_end
