c80f13ec99cf04c1fd1c04acb6b385a41f6c6218
eitan <devnull@localhost>
2013-01-18 01:23:37 -0500

    fix uninstaller comment

--- uninstaller.sh
+++ uninstaller.sh
@@ -1,7 +1,8 @@
 #! /bin/sh -x
 self=$(realpath ~/.conf);
 
-#Test ln. only link if the second argument doesn't exist
+#Test ln remover - only remove items if they are symlinks.
+#Sanity checks to make sure they reside in a specific directory may be useful.
 begon () {
 	[ -L "$1" ] && rm "$1";
 }
