47551d2d32d5aa2f632f1969fa0a6398f93d3ec6
Eitan Adler <lists@eitanadler.com>
2013-01-25 09:26:04 -0500

    both work, but prefer with a dash

--- zsh/env.sh
+++ zsh/env.sh
@@ -1,7 +1,7 @@
 export BLOCKSIZE=K;
 export EDITOR=vim;
 export PAGER=less;
-export LESS="RM"
+export LESS="-RM"
 
 export PATH="/usr/local/libexec/ccache:$PATH:$HOME/bin";
 export CCACHE_PATH=/usr/local/bin:/usr/bin
