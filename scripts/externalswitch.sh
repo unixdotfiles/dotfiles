5fd83d221cda641bca15b4cd880ad0d7015a2ba3
Eitan Adler <lists@eitanadler.com>
2013-09-02 16:56:32 -0400

    Forgot to add this script.

---
+++ scripts/externalswitch.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+EXTERNAL="$1"
+
+connectedOutputs=$(xrandr | grep " connected" | cut -w -f 1)
+activeOutput=$(xrandr | grep -e " connected [^(]" | sed -e "s/\([A-Z0-9]\+\) connected.*/\1/" | cut -w -f 1)
+
+if [ "${activeOutput#*$EXTERNAL}" == "$activeOutput" ]
+then
+	xrandr --output "$EXTERNAL" --auto
+	xset -dpms
+else
+	xrandr --output "$EXTERNAL" --off
+
+fi
