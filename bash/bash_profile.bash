6d1623c3022b4def5c4a43aca204ace419b617d0
eitan <devnull@localhost>
2013-09-15 12:33:27 -0400

    Update, modify, and fix the bash config files.  These were starting to become annoying as misconfigured.

--- bash/bash_profile.bash
+++ bash/bash_profile.bash
@@ -1,9 +1,8 @@
-return
-if [ -t 0 ]
+# Interactive Login Shell or non-interactive shell
+if [[ $- == *i* ]]
 then
 	[ -e /usr/bin/zsh ] && exec /usr/bin/zsh
 	[ -e /bin/zsh ] && exec /bin/zsh
 	[ -e /usr/local/bin/zsh ] && exec /usr/local/bin/zsh
 fi
-
-
+source ~/.bashrc
