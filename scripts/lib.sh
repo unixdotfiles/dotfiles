ba26e33b8d3b32e1c0853dc8d77f004ae96a9203
Eitan Adler <lists@eitanadler.com>
2013-12-07 23:38:24 -0500

    Add configuration parameter to library.

--- scripts/lib.sh
+++ scripts/lib.sh
@@ -1,7 +1,9 @@
 . ~/.conf/zsh/exists.func
 
+: ${LIBSH_ALERT_TIME:=3000}
+
 alert() {
 	text="$1"; shift;
 	__exists Xdialog || return
-	(Xdialog --title "pcm sound" --infobox "$text" 0 0 3000 &)
+	(Xdialog --title "pcm sound" --infobox "$text" 0 0 $LIBSH_ALERT_TIME &)
 }
