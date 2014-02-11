7edb930ec784ea5617af06104bcb20c4f3baef66
Eitan Adler <lists@eitanadler.com>
2014-02-11 18:13:39 -0500

    Add some better behavior for the script.

--- freebsd/global/copy-check.sh
+++ freebsd/global/copy-check.sh
@@ -6,7 +6,7 @@ files="
 
 for f in $files
 do
-	echo cp $f $(printf "%s" $f | sed 's,/,-,g')
+	cp $f $(printf "%s" $f | sed 's,/,-,g')
 done
 
-hg diff -q . && hg ci .
+hg diff -q . && (echo "Press any key to continue or C-c to cancel" && read _) && hg ci .
