ffa024c12aca228eb331afd3453d4701645fc832
eitan <devnull@localhost>
2014-02-11 18:15:31 -0500

    Don't include the initial '-'

--- freebsd/global/copy-check.sh
+++ freebsd/global/copy-check.sh
@@ -6,7 +6,7 @@ files="
 
 for f in $files
 do
-	cp $f $(printf "%s" $f | sed 's,/,-,g')
+	cp $f $(printf "%s" ${f#/} | sed 's,/,-,g')
 done
 
 hg diff -q . && (echo "Press any key to continue or C-c to cancel" && read _) && hg ci .
