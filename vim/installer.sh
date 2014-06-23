c93f44d7a5c5c81f099a4fcd38ebfb6e0208b384
Eitan Adler <lists@eitanadler.com>
2014-06-23 13:01:59 -0700

    let vimrc manually handle these for now: no need for installer!

--- vim/installer.sh
+++ vim/installer.sh
@@ -1,8 +1,5 @@
 if ! __isroot
 then
     [ ! -d ~/.vim/bundle/vundle ] && git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
-    mkdir -p ~/.vim/backup
-    mkdir -p ~/.vim/undo
-    mkdir -p ~/.vim/swap
     mkdir -p ~/.vim/viminfo
 fi
