04244b01ac792bb3e890c5732d5a649c68e9a785
Eitan Adler <lists@eitanadler.com>
2014-06-26 23:59:53 -0700

    Handle more cases in start-tmux.sh

--- zsh/start-tmux.sh
+++ zsh/start-tmux.sh
@@ -4,12 +4,16 @@ __tmux_begin() {
 	# Don't let tmux run except in a child shell.
 	# This allows me to run X outside of tmux and have
 	# tmux run inside each tmux terminal
-	if __exists tmux && [ -n "$__ZSHRC_FIRST_RUN" ] && [ -z "$TMUX" ]
+
+	if __exists tmux && ! [ -z "$__ZSHRC_FIRST_RUN" -a -n "$__zshrc_tmux_childonly" ] && [ -z "$TMUX" ]
     then
+        # If we want to automatically attach to existing session - do so
+        [ -n "$__zshrc_auto_attach" ] && tmux has-session && exec tmux attach
+
+        # If we want to start tmux normally, do so.
         exec tmux
-    else
-        export __ZSHRC_FIRST_RUN="ready";
-	fi
+    fi
+    export __ZSHRC_FIRST_RUN="ready";
 }
 
 [ -z "$__zshrc_tmux_disable" ] && __tmux_begin
