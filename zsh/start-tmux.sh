eeb5d46b0c883590e9d4ce0cc31b8e79d908bfb9
Eitan Adler <lists@eitanadler.com>
2015-04-12 11:13:36 -0700

    - be more explicit about starting tmux in non-child envs, when the   childonly param is not set - set env before starting tmux as PATH may not be found otherwise
    I need to migrate this to a '#.name' format rather than explicit
    ordering

--- zsh/start-tmux.sh
+++ zsh/start-tmux.sh
@@ -1,15 +1,19 @@
 #!/bin/sh
 
 __tmux_begin() {
-	if __exists tmux && ! [ -z "$__ZSHRC_FIRST_RUN" -a -n "$__zshrc_tmux_childonly" ] && [ -z "$TMUX" ]
+	if __exists tmux && [ -z "$TMUX" ]
     then
-        # If we want to automatically attach to existing session - do so
-        [ -n "$__zshrc_auto_attach" ] && tmux has-session && exec tmux attach
+        if [ -n "$__zshrc_tmux_childonly" -a -n "$__ZSHRC_FIRST_RUN" ]
+           [ -z "$__zshrc_tmux_childonly" ]
+        then
+            # If we want to automatically attach to existing session - do so
+            [ -n "$__zshrc_auto_attach" ] && tmux has-session && exec tmux attach
 
-        # If we want to start tmux normally, do so.
-        exec tmux
+            # If we want to start tmux normally, do so.
+            exec tmux
+        fi
     fi
-    export __ZSHRC_FIRST_RUN="ready";
 }
 
+export __ZSHRC_FIRST_RUN="ready";
 [ -z "$__zshrc_tmux_disable" ] && __tmux_begin && __tmux_restore_env
