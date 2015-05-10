be4379afd7967e44e9d4e0a20efb9c78194ccf0e
Eitan Adler <lists@eitanadler.com>
2015-05-10 10:55:57 -0700

    get the logic for starting tmux in the various cases actually correct

--- zsh/start-tmux.sh
+++ zsh/start-tmux.sh
@@ -3,7 +3,8 @@
 __tmux_begin() {
 	if __exists tmux && [ -z "$TMUX" ]
     then
-        if [ -n "$__zshrc_tmux_childonly" -a -n "$__ZSHRC_FIRST_RUN" ]
+        if ([ -n "$__zshrc_tmux_childonly" ] && [ -n "$__ZSHRC_FIRST_RUN" ]) ||
+            [ -z "$__zshrc_tmux_childonly" ]
         then
             # If we want to automatically attach to existing session - do so
             [ -n "$__zshrc_auto_attach" ] && tmux has-session && exec tmux attach
@@ -14,5 +15,5 @@ __tmux_begin() {
     fi
 }
 
-export __ZSHRC_FIRST_RUN="ready";
 [ -z "$__zshrc_tmux_disable" ] && __tmux_begin && __tmux_restore_env
+export __ZSHRC_FIRST_RUN="ready";
