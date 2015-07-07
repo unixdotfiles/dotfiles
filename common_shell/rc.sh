96d849371314f7071a7e895cb6a2c5673c0eb599
Eitan Adler <lists@eitanadler.com>
2015-07-07 10:44:14 -0700

    migrate tmux starting to common_shell
    one less zsh or bash specific thing
    also add $__INTSHELL to figure out the currently running shell

--- common_shell/rc.sh
+++ common_shell/rc.sh
@@ -5,11 +5,16 @@ export _confpath=~/.conf/common_shell
 __HOSTNAME="$(hostname)"
 HOST="${__HOSTNAME%%.*}"
 
+__INTSHELL="sh";
+[ -n "$BASH_VERSION" ] && __INTSHELL="bash"
+[ -n "$ZSH_VERSION" ] && __INTSHELL="zsh"
+
 uname_s=$(uname -s)
 
 source $_confpath/functions.sh
 msource /etc/environment
 
 source $_confpath/env.sh
+source $_confpath/start-tmux.sh
 source $_confpath/aliases.sh
 source $_confpath/extra/eval_spin.func
---
+++ common_shell/start-tmux.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+__tmux_begin() {
+	if __exists tmux && [ -z "$TMUX" ]
+    then
+        if ([ -n "$__shellrc_tmux_childonly" ] && [ -n "$__SHELLRC_FIRST_RUN" ]) ||
+            [ -z "$__shellrc_tmux_childonly" ]
+        then
+            # If we want to automatically attach to existing session - do so
+            [ -n "$__shellrc_auto_attach" ] && tmux has-session && exec tmux attach
+
+            # If we want to start tmux normally, do so.
+            exec tmux
+        fi
+    fi
+}
+
+[ -z "$__shellrc_tmux_disable" ] && __tmux_begin && __tmux_restore_env
+export __SHELLRC_FIRST_RUN="ready";
