53d5012776b9c58caab2cb83a136549f4e8061b5
Eitan Adler <lists@eitanadler.com>
2015-07-13 09:31:11 -0700

    Prefer $HOME/bin over other options

--- common_shell/env.sh
+++ common_shell/env.sh
@@ -2,7 +2,7 @@ export BLOCKSIZE=K;
 export EDITOR=vim;
 export PAGER=less;
 
-export PATH="/usr/local/libexec/ccache:/opt/local/bin:/opt/local/sbin:$PATH:$HOME/bin:$HOME/svn/arcanist/bin";
+export PATH="$HOME/bin:/usr/local/libexec/ccache:/opt/local/bin:/opt/local/sbin:$PATH:$HOME/svn/arcanist/bin";
 export CCACHE_PATH=/usr/local/bin:/usr/bin
 export CCACHE_DIR=/cache/ccache
 export FORTUNE_PATH="/usr/share/games/fortune:/usr/local/share/games/fortune:$HOME/.fortune";
