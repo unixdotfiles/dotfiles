662c35e2cb89c86ae935bfb53be19351085693c3
Eitan Adler <lists@eitanadler.com>
2015-08-21 11:30:11 -0700

    better code hygine; try to keep number of files in RPROMPT

--- zsh/ps1.zsh
+++ zsh/ps1.zsh
@@ -49,30 +49,32 @@ PS1_WD="%F{magenta}%30<...<%~";	    #current working directory limited to 30 cha
 
 PS1_VIM="%F{green}${VIMRUNTIME:+vim}%f"
 
-function setCurrentPS1()
+setCurrentPS1()
 {
-	PS1_BATTERY=""
+  local $(stat -Ls .)A
+  local PS1_N_FILES="[$(( $st_nlink - 1 )) files]"
+	local PS1_BATTERY=""
 	[ "$uname_s" = "FreeBSD" -a -n "$__shellrc_battery" ] && PS1_BATTERY="%F{yellow}($(sysctl -n hw.acpi.battery.life)%%)"
-	PS1_VCS="%F{green}$(__vcs_dir)";	#info about the vcs
-	PS1="[$PS1_HIST $PS1_USER@$PS1_HOST $PS1_BATTERY $PS1_EC2 $PS1_WD $PS1_ERR%f]%#"
-	RPS1_TIMER=""
+	local PS1_VCS="%F{green}$(__vcs_dir)";	#info about the vcs
+  PS1="[$PS1_HIST $PS1_USER@$PS1_HOST $PS1_BATTERY $PS1_EC2 $PS1_WD $PS1_ERR%f]%#"
+	local RPS1_TIMER=""
 	if [ $__cmd_exec_timer ]
 	then
-		__cmd_exec_timediff="$(($SECONDS - $__cmd_exec_timer))"
+		local __cmd_exec_timediff="$(($SECONDS - $__cmd_exec_timer))"
 		if [ $__cmd_exec_timediff -gt 2 ]
 		then
-			RPS1_TIMER="%F{cyan}${__cmd_exec_timediff}s%f "
+			local RPS1_TIMER="%F{cyan}${__cmd_exec_timediff}s%f "
 		fi
 		unset __cmd_exec_timer
 	fi
-	_VENV_NAME="${VIRTUAL_ENV##*/}"
-	_COLOR_VENV_NAME="(%F{blue}${_VENV_NAME}%f)"
-	_VENV_VCS_SPACE="${PS1_VCS:+ }"
-	RPS1_PYTHON="${_VENV_NAME:+$_COLOR_VENV_NAME}$_VENV_VCS_SPACE"
-	RPS1="$RPS1_TIMER${VIMRUNTIME:+"{$PS1_VIM}"}${RPS1_PYTHON}$PS1_VCS%f";
+	local _VENV_NAME="${VIRTUAL_ENV##*/}"
+	local _COLOR_VENV_NAME="(%F{blue}${_VENV_NAME}%f)"
+	local _VENV_VCS_SPACE="${PS1_VCS:+ }"
+	local RPS1_PYTHON="${_VENV_NAME:+$_COLOR_VENV_NAME}$_VENV_VCS_SPACE"
+	RPS1="$RPS1_TIMER $PS1_N_FILES ${VIMRUNTIME:+"{$PS1_VIM}"}${RPS1_PYTHON}$PS1_VCS%f";
 }
 
-function setExecutionTimer() {
+setExecutionTimer() {
 	__cmd_exec_timer=${__cmd_exec_timer:-$SECONDS}
 }
 
