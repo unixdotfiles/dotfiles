4b57318dd5d1f093f8637b9dad37a3e549613de9
Eitan Adler <lists@eitanadler.com>
2015-09-14 09:56:13 -0700

    Simplify alias creation   - use newly created 'redefine'   - move things around to make them easier to find

--- common_shell/aliases.sh
+++ common_shell/aliases.sh
@@ -1,6 +1,3 @@
-alias mv="mv -i";
-alias cp="cp -i";
-
 # Enable ls colors
 if [ -z "$DISABLE_LS_COLORS" ]
 then
@@ -23,36 +20,40 @@ fi
 ! __exists fetch && __exists wget && alias fetch=wget
 
 __exists vim && alias vi="vim -p";
-__exists vim && alias vim="vim -p";
-
-__exists xterm && alias cterm="xterm -C -T 'Console Eterm'";
+redefine vim="vim -p";
 
 __exists idesk && alias switch-background="pkill idesk && idesk &";
-__exists youtube-dl && alias youtube-dl="youtube-dl -t";
-alias startx="startx -audit 4 -nolisten tcp";
+
 alias sgrep='grep --exclude "*svn*" -I';
-__exists portmaster && alias portmaster="portmaster -m'-DNO_DEPENDS'";
-__exists urxvt && alias urxvt="urxvtcd"
-__exists proxychains && alias pc="proxychains"
+
 alias myip="curl ifconfig.pro";
 
+alias mv="mv -i";
+alias cp="cp -i";
+alias ll="ls -halt"
 alias lls="ls -lao";
 alias tolower="tr A-Z a-z"
 alias toupper="tr a-z A-Z"
 alias rot13="tr [A-Za-z] [N-ZA-Mn-za-m]"
-__exists pastebinit && alias pastebinit="pastebinit -a ''"
-__exists portlint && alias portlint="portlint -C";
 alias sprunge='curl -F '\''sprunge=<-'\'' http://sprunge.us'
 __exists curl && dict() { curl "dict://dict.org/d:$1"; }
-__exists hub && alias git="hub"
+__exists hub && redefine git="hub"
 
-alias diff="diff -p";
-
-alias ll="ls -halt"
+redefine diff="diff -p";
+redefine pastebinit="pastebinit -a ''"
+redefine portlint="portlint -C";
+redefine youtube-dl="youtube-dl -t";
+redefine startx="startx -audit 4 -nolisten tcp";
+redefine portmaster="portmaster -m'-DNO_DEPENDS'";
+redefine urxvt="urxvtcd"
+redefine cscope="cscope -CqRv";
+redefine txt2regex="txt2regex --whitebg";
+redefine kinit="kinit --keychain"
+redefine swipl="swipl -q"
+redefine R="R --save --quiet"
 
 alias timestamp='date +%s'
 
-#alias cd="pushd";
 alias p="pushd";
 alias b="popd";
 
@@ -62,15 +63,8 @@ alias rw-="chmod 600"
 alias r-x="chmod 755"
 alias r--="chmod 644"
 
-__exists cscope && alias cscope="cscope -CqRv";
-__exists txt2regex && alias txt2regex="txt2regex --whitebg";
 __exists ack-grep && alias ack="ack-grep"
 __exists gpg2 && ! __exists gpg && alias gpg=gpg2
-
-__exists kinit && alias kinit="kinit --keychain"
-
-__exists swipl && alias swipl="swipl -q"
-__exists R && alias R="R --save --quiet"
 
 text_textbelt() {
     number="$1"
