dee2a3397cb9490d3d0f1e8015161a00b7ecebbb
Eitan Adler <lists@eitanadler.com>
2015-09-26 19:22:21 -0700

    Copy pasta: use the same format for everything

--- zsh/keybinding.zsh
+++ zsh/keybinding.zsh
@@ -6,12 +6,10 @@ bindkey -e
 [[ -z "$terminfo[kpp]"   ]]  || bindkey -M emacs "$terminfo[kpp]"   up-line-or-history
 [[ -z "$terminfo[knp]"   ]]  || bindkey -M emacs "$terminfo[knp]"   down-line-or-history
 [[ -z "$terminfo[kich1]" ]]  || bindkey -M emacs "$terminfo[kich1]" overwrite-mode
+[[ -z "$terminfo[kLFT5]" ]] || bindkey -M emacs $terminfo[kLFT5] backward-word # C-left
+[[ -z "$terminfo[kRIT5]" ]] || bindkey -M emacs $terminfo[kRIT5] forward-word # C-right
 
 [[ -z "$terminfo[kich1]" ]]  || bindkey -M vicmd "$terminfo[kich1]" overwrite-mode
 #[[ "$terminfo[kcuu1]" == "^[O"* ]] && bindkey -M viins "${terminfo[kcuu1]/O/[}" vi-up-line-or-history
-
-# xterm extensions
-bindkey $terminfo[kLFT5] backward-word # C-left
-bindkey $terminfo[kRIT5] forward-word # C-right
 
 bindkey '^Xh' _complete_help
