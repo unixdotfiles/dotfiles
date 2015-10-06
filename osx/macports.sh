aea4d85dc8221ea1cca6ccd35cd49cbc24b1168d
Eitan Adler <lists@eitanadler.com>
2015-10-05 23:02:45 -0700

    Add basic macports script: should be run separate from installer since it presumes macports exists and isn't silent

---
+++ osx/macports.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+set -x
+
+# Ask for the administrator password upfront
+sudo -v
+
+sudo port select --set python python34
+sudo port select --set python2 python27
+sudo port select --set python3 python34
+sudo port select --set ruby ruby20
+sudo port select --set cython cython27
+
+port select --summary
