ac423ef482aa6f67c5d5668a4a3655cd68f35550
Eitan Adler <lists@eitanadler.com>
2015-10-06 12:44:27 -0700

    I use my own prompt for ves

--- common_shell/env.sh
+++ common_shell/env.sh
@@ -70,5 +70,6 @@ export LANG=en_US.UTF-8
 export CONF_PATH=~/.conf/
 export MYSQL_PS1="\c \u@\h:\p [\d]>"
 export WORKON_HOME=~/.ve
+export VIRTUAL_ENV_DISABLE_PROMPT=1
 # Show time(1) like information for long running commands
 export REPORTTIME=2
