2b1d5a1e6ec6bbb7ae0dcbb970b526aa1cdcef51
Eitan Adler <lists@eitanadler.com>
2015-10-05 23:03:35 -0700

    Add scala; sort.

--- osx/macports.sh
+++ osx/macports.sh
@@ -4,10 +4,11 @@ set -x
 # Ask for the administrator password upfront
 sudo -v
 
+sudo port select --set cython cython27
 sudo port select --set python python34
 sudo port select --set python2 python27
 sudo port select --set python3 python34
 sudo port select --set ruby ruby20
-sudo port select --set cython cython27
+sudo port select --set scala scala2.11
 
 port select --summary
