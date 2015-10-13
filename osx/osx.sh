8198c7c1e5bba4db9d632d6e889bf234fc9e40b0
Eitan Adler <lists@eitanadler.com>
2015-10-13 08:43:16 -0700

    merge osx and macports.  It may require rerunning but meh

--- osx/osx.sh
+++ osx/osx.sh
@@ -130,3 +130,22 @@ defaults write org.m0k.transmission WarningLegal -bool false
 ###############################################################################
 
 git config --global --replace-all credential.helper osxkeychain
+
+###############################################################################
+# macports
+###############################################################################
+
+sudo port select --set cython cython27
+sudo port select --set python python34
+sudo port select --set python2 python27
+sudo port select --set python3 python34
+sudo port select --set ruby ruby20
+sudo port select --set scala scala2.11
+sudo port select --set pip pip34
+sudo port select --set ipython py34-ipython
+sudo port select --set ipython2 py27-ipython
+sudo port select --set ipython3 py34-ipython
+sudo port select --set gcc mp-gcc49
+sudo port select --set llvm mp-llvm-3.5
+
+port select --summary
