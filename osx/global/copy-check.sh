1dea2e26cc83e99bbed9d1fb73b7cb6a38a2bd91
Eitan Adler <lists@eitanadler.com>
2016-02-15 08:55:33 -0800

    Add php.ini

--- osx/global/copy-check.sh
+++ osx/global/copy-check.sh
@@ -3,6 +3,7 @@ files="
 /opt/local/etc/macports/macports.conf
 /opt/local/etc/macports/sources.conf
 /opt/local/etc/macports/variants.conf
+/opt/local/etc/php70/php.ini
 /etc/exports
 /etc/launchd.conf
 /etc/sysctl.conf
@@ -10,7 +11,7 @@ files="
 
 for f in $files
 do
-	cp $f $(printf "%s" ${f#/} | sed 's,/,-,g')
+	cp "$f" $(printf "%s" ${f#/} | sed 's,/,-,g')
 done
 
 git diff --exit-code . || (echo "Press any key to continue or C-c to cancel" && read _) && git ci .
