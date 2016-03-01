b656699e9f35532a862b9b2936182eddb33acbbd
Eitan Adler <lists@eitanadler.com>
2016-02-29 23:45:11 -0800

    local::lib is terrible. Just use sudo.

--- common_shell/env.sh
+++ common_shell/env.sh
@@ -70,10 +70,6 @@ export VIRTUAL_ENV_DISABLE_PROMPT=1
 export PYTHONIOENCODING=utf-8
 export R_LIBS=~/.R
 
-export PERL5LIB="$HOME/perl5/lib/perl5${PERL5LIB+:}${PERL5LIB}"
-export PERL_LOCAL_LIB_ROOT="$HOME/perl5${PERL_LOCAL_LIB_ROOT+:}${PERL_LOCAL_LIB_ROOT}"
-export PERL_MB_OPT="--install_base \"$HOME/perl5\""
-export PERL_MM_OPT="INSTALL_BASE=$HOME/perl5"
 __exists curl && __EC2_IID="$(curl -sf --connect-timeout .1 http://169.254.169.254/latest/meta-data/instance-id)"
 
 # Show time(1) like information for long running commands
