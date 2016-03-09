f90180c115a48bf9cc4d04c7bba8cee850eee0d4
Eitan Adler <lists@eitanadler.com>
2016-03-08 23:41:09 -0800

    bump pip version

--- python/install.sh
+++ python/install.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 pip-2.7 install --user -U -r global.requirements.txt -r 27.requirements.txt
-pip-3.4 install --user -U -r global.requirements.txt -r 3.requirements.txt
+pip-3.5 install --user -U -r global.requirements.txt -r 3.requirements.txt
