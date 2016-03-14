bdd2fa5c46070db28c32b8189d4f2a9a03bdb9e0
Eitan Adler <lists@eitanadler.com>
2016-03-13 21:44:45 -0700

    Add apm

--- packages/install.sh
+++ packages/install.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 sudo -H gem install $(<gem)
 npm install -g $(<npm)
+apm install $(<apm)
 pip-2.7 install --user -U -r global.requirements.txt -r 27.requirements.txt
 pip-3.5 install --user -U -r global.requirements.txt -r 3.requirements.txt
