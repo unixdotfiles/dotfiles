780aa7e30a7542a795ffe2fde8536c619ee3f6dd
Eitan Adler <lists@eitanadler.com>
2016-03-24 16:51:45 -0700

    rm more python

--- common_shell/rc.sh
+++ common_shell/rc.sh
@@ -21,7 +21,7 @@ msource /etc/environment
 . $_shellpath/ps1.$__INTSHELL
 [ "$TERM_PROGRAM" = "iTerm.app" ] && msource $_confpath/extra/iterm2_shell_integration.$__INTSHELL
 
-msource /opt/local/Library/Frameworks/Python.framework/Versions/3.5/bin/virtualenvwrapper.sh
+msource /Users/eax/Library/Python/2.7/bin/virtualenvwrapper.sh
 
 for fle in $__rc_shell_files
 do
