1b29675bc232d7c0e70e556c09cbaa2c998b83e6
Eitan Adler <lists@eitanadler.com>
2016-04-26 09:29:02 -0700

    add cabal install?

--- packages/install.sh
+++ packages/install.sh
@@ -4,4 +4,5 @@ npm install -g $(<npm)
 apm install $(<apm)
 pip-2.7 install --user -U -r global.requirements.txt -r 27.requirements.txt
 pip-3.5 install --user -U -r global.requirements.txt -r 3.requirements.txt
+cabal update && cabal install $(<cabal)
 # cpanm -S -n $(<cpan)
