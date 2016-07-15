8beea324305dc8cd57f0e05c230b6948d6023d03
Eitan Adler <lists@eitanadler.com>
2016-07-15 14:46:44 -0700

    complete git imerge too

--- zsh/complete.zsh
+++ zsh/complete.zsh
@@ -71,7 +71,7 @@ zstyle ':completion::complete:ldapsearch:*' tag-order '! globbed-files' -
 #zstyle ':completion:*' file-patterns \
 #    '%p:globbed-files: *(-/):directories:Directories' '*:all-files'
 
-__exists git-last-modified && zstyle ':completion:*:*:git:*' user-commands last-modified:'shows when files were last modified'
+__exists git-last-modified && zstyle ':completion:*:*:git:*' user-commands last-modified:'shows when files were last modified' imerge:'interactive merge'
 
 __exists hub git && compdef hub=git
 __exists gpg2 && ! (( $+_comps[gpg2] )) && compdef gpg2=gpg
