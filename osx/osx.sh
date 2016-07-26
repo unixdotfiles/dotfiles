b03ffbbd66de1828ac1d4ee04a84312357c63651
Eitan Adler <lists@eitanadler.com>
2016-07-26 15:05:15 -0700

    make it less likely for port selection to fail

--- osx/osx.sh
+++ osx/osx.sh
@@ -137,32 +137,4 @@ defaults write org.m0k.transmission WarningLegal -bool false
 
 __exists git && git config --global --replace-all credential.helper osxkeychain
 
-###############################################################################
-# macports
-###############################################################################
-
-if __exists port
-then
-sudo port select --set cython cython35
-sudo port select --set maven maven3
-sudo port select --set python python35
-sudo port select --set python2 python27
-sudo port select --set python3 python35
-sudo port select --set ruby ruby22
-sudo port select --set scala scala2.11
-sudo port select --set pip pip35
-sudo port select --set php php70
-sudo port select --set ipython py35-ipython
-sudo port select --set ipython2 py27-ipython
-sudo port select --set ipython3 py35-ipython
-sudo port select --set gcc mp-gcc5
-sudo port select --set llvm mp-llvm-3.7
-sudo port select --set mysql mariadb-10.1
-sudo port select --set postgresql_select postgresql95
-sudo port select --set perl perl5.18-apple.15
-sudo port select --set nosetests nosetests35
-sudo port select --set postgresql postgresql95
-sudo port select --set virtualenv virtualenv35
-
-port select --summary
-fi
+~/.conf/osx/osx_select.sh
---
+++ osx/osx_select.sh
@@ -0,0 +1,31 @@
+. ../common_shell/functions.sh
+
+if __exists port
+then
+sudo port select --set cython cython27
+sudo port select --set cython cython35
+sudo port select --set maven maven3
+sudo port select --set python python35
+sudo port select --set python2 python27
+sudo port select --set python3 python35
+sudo port select --set ruby ruby20
+sudo port select --set ruby ruby22
+sudo port select --set scala scala2.11
+sudo port select --set pip pip27
+sudo port select --set pip pip35
+sudo port select --set php php70
+sudo port select --set ipython py35-ipython
+sudo port select --set ipython2 py27-ipython
+sudo port select --set ipython3 py35-ipython
+sudo port select --set gcc mp-gcc5
+sudo port select --set llvm mp-llvm-3.7
+sudo port select --set mysql mariadb-10.1
+sudo port select --set postgresql_select postgresql95
+sudo port select --set perl perl5.18-apple.15
+sudo port select --set nosetests nosetests27
+sudo port select --set nosetests nosetests35
+sudo port select --set postgresql postgresql95
+sudo port select --set virtualenv virtualenv35
+
+port select --summary
+fi
