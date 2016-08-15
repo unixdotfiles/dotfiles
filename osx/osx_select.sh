12ece40bf16ab4d56bfd57dcc7f904861edfc8dc
Eitan Adler <lists@eitanadler.com>
2016-08-15 02:53:34 -0400

    bump ruby version - fallback not required

--- osx/osx_select.sh
+++ osx/osx_select.sh
@@ -8,8 +8,7 @@ sudo port select --set maven maven3
 sudo port select --set python python35
 sudo port select --set python2 python27
 sudo port select --set python3 python35
-sudo port select --set ruby ruby20
-sudo port select --set ruby ruby22
+sudo port select --set ruby ruby23
 sudo port select --set scala scala2.11
 sudo port select --set pip pip27
 sudo port select --set pip pip35
