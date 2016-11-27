98227910bf230ad6072a683d662e967b94b85b16
Eitan Adler <lists@eitanadler.com>
2016-11-26 19:51:24 -0800

    lets reduce the number of package managers I care about

--- packages/install.sh
+++ packages/install.sh
@@ -7,5 +7,4 @@ apm install $(<apm)
 pip-2.7 install --user -U -r global.requirements.txt -r 27.requirements.txt
 pip-3.5 install --user -U -r global.requirements.txt -r 3.requirements.txt
 cabal update && cabal install $(<cabal)
-cargo install $(<cargo)
 # cpanm -S -n $(<cpan)
