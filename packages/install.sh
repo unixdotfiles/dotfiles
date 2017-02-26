dd4156ccb187c00b85e9d9881b43eb0aec4db221
Eitan Adler <lists@eitanadler.com>
2017-02-25 19:32:08 -0800

    use stack to install hs files

--- packages/install.sh
+++ packages/install.sh
@@ -6,4 +6,5 @@ sudo npm install -g $(<npm)
 apm install $(<apm)
 pip-2.7 install --user -U -r global.requirements.txt -r 27.requirements.txt
 pip-3.5 install --user -U -r global.requirements.txt -r 3.requirements.txt
+stack install $(<cabal)
 # cpanm -S -n $(<cpan)
