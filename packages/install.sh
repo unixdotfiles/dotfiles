2cd6370c228754c75f99038899e46cd5e35a4266
Eitan Adler <lists@eitanadler.com>
2017-04-17 23:56:46 -0700

    we use sudo for privsep but we want to install locally; should likely just remove privsep entirely

--- packages/install.sh
+++ packages/install.sh
@@ -2,7 +2,7 @@
 gem install --user-install $(<gem)
 gem_user_dir=$(ruby -e 'puts Gem.user_dir')
 [ -f "$gem_user_dir/bin/t" ] && mv "$gem_user_dir/bin/t" "$gem_user_dir/bin/twttr"
-sudo npm install -g $(<npm)
+sudo -H npm install -g $(<npm)
 apm install $(<apm)
 pip-2.7 install --user -U -r global.requirements.txt -r 27.requirements.txt
 pip-3.6 install --user -U -r global.requirements.txt -r 3.requirements.txt
