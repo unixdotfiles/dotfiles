2d1266d7ab423d5ebf13ebbd444400724903eac2
Eitan Adler <lists@eitanadler.com>
2017-09-11 08:21:47 -0700

    Update pin to mp-llvm-4.0

--- osx/osx_select.sh
+++ osx/osx_select.sh
@@ -17,7 +17,7 @@ sudo port select --set ipython py36-ipython
 sudo port select --set ipython2 py27-ipython
 sudo port select --set ipython3 py36-ipython
 sudo port select --set gcc mp-gcc6
-sudo port select --set llvm mp-llvm-3.9
+sudo port select --set llvm mp-llvm-4.0
 sudo port select --set mysql mariadb-10.1
 sudo port select --set postgresql_select postgresql95
 sudo port select --set perl perl5.18-apple.15
