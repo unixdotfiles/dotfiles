ae1a60b2b3bf50c2dabcc95364ab0a804b042e75
Eitan Adler <lists@eitanadler.com>
2017-09-17 01:05:58 -0700

    Use standard commit name

--- osx/global/copy-check.sh
+++ osx/global/copy-check.sh
@@ -6,4 +6,4 @@ do
 	cp "$f" $(printf "%s" ${f#/} | sed 's,/,-,g')
 done
 
-git diff --exit-code . || (echo "Press any key to continue or C-c to cancel" && read _) && git ci .
+git diff --exit-code . || (echo "Press any key to continue or C-c to cancel" && read _) && git commit .
