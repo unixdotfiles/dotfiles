3c6e7a8d5197471e46a4e86decf6d0d6365edf9a
Eitan Adler <lists@eitanadler.com>
2017-10-08 23:19:40 -0700

    Add additional settings for Textual

--- osx/third_party.sh
+++ osx/third_party.sh
@@ -13,7 +13,9 @@ then
 fi
 
 defaults write com.codeux.apps.textual ApplicationCTCPVersionMasquerade -string "telnet"
-defaults write com.codeux.apps.textual "ApplyCommandToAllConnections -> clearall" -int 0
+defaults write com.codeux.apps.textual "ApplyCommandToAllConnections -> clearall" -bool false
 defaults write com.codeux.apps.textual "DefaultIdentity -> Realname" -string "Charlie Root"
 defaults write com.codeux.apps.textual "DefaultIdentity -> Username" -string "root"
+defaults write com.codeux.apps.textual "Keyboard -> Tab Key Completion Suffix" -string ": "
 defaults write com.codeux.apps.textual ScrollbackMaximumLineCount -int 16000
+defaults write com.codeux.apps.textual TextualDeveloperEnvironment -bool true
