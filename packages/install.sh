26fec4d4fd8f5d13489f2458560c9e4d5de57fe2
Eitan Adler <lists@eitanadler.com>
2017-10-12 23:10:38 -0700

    Go back to stock cpan
    cpan confuses the heck out of me, but simple works

--- packages/install.sh
+++ packages/install.sh
@@ -7,4 +7,4 @@ apm install $(<apm)
 pip-2.7 install --user -U -r global.requirements.txt -r 27.requirements.txt
 pip-3.6 install --user -U -r global.requirements.txt -r 3.requirements.txt
 stack install $(<cabal)
-# cpanm -S -n $(<cpan)
+cpan install $(<cpan)
