f201ad992c41812005e9f67b326d5283d3335964
Eitan Adler <lists@eitanadler.com>
2017-12-16 14:09:16 -0800

    print diff before overwriting files

--- osx/global/restore.sh
+++ osx/global/restore.sh
@@ -5,9 +5,10 @@ set -x
 
 for f in $files
 do
-  backup="$(printf "%s" ${f#/} | sed 's,/,-,g')"
+  backup="$(printf "%s" "${f#/}" | sed 's,/,-,g')"
   if ! cmp -s "$backup" "$f"
   then
+    diff -u "$f" "$backup"
     sudo cp "$backup" "$f"
   fi
 done
