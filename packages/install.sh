ca5c4addc407d9988c3ec5f46586c7be09d87c2f
Eitan Adler <lists@eitanadler.com>
2018-05-05 14:25:29 +0000

    Make shell scripts more posixy

--- packages/install.sh
+++ packages/install.sh
@@ -5,15 +5,16 @@ quit() {
 
 trap quit INT
 
-gem install --user-install $(<gem)
+gem install --user-install $(cat gem)
 gem_user_dir=$(ruby -e 'puts Gem.user_dir')
-npm install -g $(<npm)
+npm install -g $(cat npm)
 if [ "$(uname -s)" = "Darwin" ]
-  apm install $(<apm)
+then
+  apm install $(cat apm)
 fi
 pip-2.7 install --user -U -r global.requirements.txt -r 27.requirements.txt
 pip-3.6 install --user -U -r global.requirements.txt -r 3.requirements.txt
-stack install $(<cabal)
-#cpan install $(<cpan)
-luarocks install $(<luarocks)
-cargo install $(<cargo)
+stack install $(cat cabal)
+#cpan install $(cat cpan)
+luarocks install $(cat luarocks)
+cargo install $(cat cargo)
