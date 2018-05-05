4776ce28be62132c340a06603bf279078173dc4c
Eitan Adler <lists@eitanadler.com>
2018-05-05 13:17:24 +0000

    Avoid executing 'apm' where its not atom related

--- packages/install.sh
+++ packages/install.sh
@@ -1,8 +1,16 @@
 #!/bin/sh
+quit() {
+  exit 1
+}
+
+trap quit INT
+
 gem install --user-install $(<gem)
 gem_user_dir=$(ruby -e 'puts Gem.user_dir')
 npm install -g $(<npm)
-apm install $(<apm)
+if [ "$(uname -s)" = "Darwin" ]
+  apm install $(<apm)
+fi
 pip-2.7 install --user -U -r global.requirements.txt -r 27.requirements.txt
 pip-3.6 install --user -U -r global.requirements.txt -r 3.requirements.txt
 stack install $(<cabal)
