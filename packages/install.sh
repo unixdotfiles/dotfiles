8e299342b15dc738c4defa6b0b702c60c3f96ff1
Eitan Adler <lists@eitanadler.com>
2018-05-05 13:12:27 +0000

    I stopped using 't' a while ago

--- packages/install.sh
+++ packages/install.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 gem install --user-install $(<gem)
 gem_user_dir=$(ruby -e 'puts Gem.user_dir')
-[ -f "$gem_user_dir/bin/t" ] && mv "$gem_user_dir/bin/t" "$gem_user_dir/bin/twttr"
 npm install -g $(<npm)
 apm install $(<apm)
 pip-2.7 install --user -U -r global.requirements.txt -r 27.requirements.txt
