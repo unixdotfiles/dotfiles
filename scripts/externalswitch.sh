08a693909b463d90c3b69807a5c0648d4187238d
Eitan Adler <lists@eitanadler.com>
2018-05-27 15:14:04 +0000

    Pet shellcheck

--- scripts/externalswitch.sh
+++ scripts/externalswitch.sh
@@ -2,7 +2,7 @@
 
 EXTERNAL="$1"
 
-activeOutput=$(xrandr | grep -e " connected [^(]" | sed -e "s/\([A-Z0-9]\+\) connected.*/\1/" | cut -w -f 1)
+activeOutput=$(xrandr | grep -e " connected [^(]" | sed -e 's/\([A-Z0-9]\+\) connected.*/\1/' | cut -w -f 1)
 
 if [ "${activeOutput#*$EXTERNAL}" = "$activeOutput" ]
 then
