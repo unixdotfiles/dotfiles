6324438dd21ca92846a8f467760076b0f99222d7
Eitan Adler <lists@eitanadler.com>
2018-07-14 21:55:37 -0700

    osx: switch more to python37; add pygments

--- osx/osx_select.sh
+++ osx/osx_select.sh
@@ -3,7 +3,7 @@
 if __exists port
 then
 sudo port select --set cython cython27
-sudo port select --set cython cython36
+sudo port select --set cython cython37
 sudo port select --set maven maven3
 sudo port select --set python python37
 sudo port select --set python2 python27
@@ -22,11 +22,12 @@ sudo port select --set mysql mariadb-10.1
 sudo port select --set postgresql_select postgresql95
 sudo port select --set perl perl5.18-apple.15
 sudo port select --set nosetests nosetests27
-sudo port select --set nosetests nosetests36
+sudo port select --set nosetests nosetests37
 sudo port select --set postgresql postgresql95
 sudo port select --set wxWidgets wxWidgets-3.0
 sudo port select --set maven maven3
 sudo port select --set sphinx py36-sphinx
+sudo port select --set pygments py36-pygments
 
 port select --summary
 fi
