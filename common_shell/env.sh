c57f813187b88989ca3cbfb9413c3e462e4ecef8
Eitan Adler <lists@eitanadler.com>
2019-03-29 23:03:31 -0700

    default to Java 12 if possible

--- common_shell/env.sh
+++ common_shell/env.sh
@@ -22,7 +22,7 @@ export FORTUNE_PATH="/usr/share/games/fortune:/usr/local/share/games/fortune:$HO
 
 if [ -z "${JAVA_HOME}" ]
 then
-  [ -e /usr/libexec/java_home ] && export JAVA_HOME=$(/usr/libexec/java_home -v ${__shellrc_java_ver:-11})
+  [ -e /usr/libexec/java_home ] && export JAVA_HOME=$(/usr/libexec/java_home -v ${__shellrc_java_ver:-12})
 fi
 
 __exists chrome && export BROWSER=$(which chrome)
