03d61b00ddbf6e58373df60232bf24c02ceb969d
Eitan Adler <lists@eitanadler.com>
2019-04-22 00:36:31 -0400

    select virtualenv; bump mp-llvm

--- osx/osx_select.sh
+++ osx/osx_select.sh
@@ -17,7 +17,7 @@ sudo port select --set ipython py37-ipython
 sudo port select --set ipython2 py27-ipython
 sudo port select --set ipython3 py37-ipython
 sudo port select --set gcc mp-gcc7
-sudo port select --set llvm mp-llvm-5.0
+sudo port select --set llvm mp-llvm-7.0
 sudo port select --set mysql mariadb-10.1
 sudo port select --set postgresql_select postgresql95
 sudo port select --set perl perl5.18-apple.15
@@ -28,6 +28,7 @@ sudo port select --set wxWidgets wxWidgets-3.0
 sudo port select --set maven maven3
 sudo port select --set sphinx py37-sphinx
 sudo port select --set pygments py37-pygments
+sudo port select --set virtualenv virtualenv37
 
 port select --summary
 fi
