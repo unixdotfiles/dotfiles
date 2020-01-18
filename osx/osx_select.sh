b0bbc947e87d11caa66d5a41080c2281e668b7a1
Eitan Adler <lists@eitanadler.com>
2020-01-18 11:23:23 -0800

    osx: move osx_select to use variables
    It is easier to change versions later. Too much duplication and I
    sometimes forget pieces

--- osx/osx_select.sh
+++ osx/osx_select.sh
@@ -1,37 +1,39 @@
 . ../common_shell/functions.sh
 
+pythonver=37
+phpver=74
+
 if __exists port
 then
 sudo port select --set cython cython27
-sudo port select --set cython cython37
+sudo port select --set cython cython${pythonver}
 sudo port select --set maven maven3
-sudo port select --set python python37
+sudo port select --set python python${pythonver}
 sudo port select --set python2 python27
-sudo port select --set python3 python37
+sudo port select --set python3 python${pythonver}
 sudo port select --set ruby ruby25
-sudo port select --set pip pip37
+sudo port select --set pip pip${pythonver}
 sudo port select --set pip2 pip27
-sudo port select --set pip3 pip37
-sudo port select --set php php73
+sudo port select --set pip3 pip${pythonver}
+sudo port select --set php php${phpver}
 sudo port select --set pip pip27
-sudo port select --set pip pip37
-sudo port select --set php php74
+sudo port select --set pip pip${pythonver}
 sudo port select --set scala scala2.13
-sudo port select --set ipython py37-ipython
+sudo port select --set ipython py${pythonver}-ipython
 sudo port select --set ipython2 py27-ipython
-sudo port select --set ipython3 py37-ipython
+sudo port select --set ipython3 py${pythonver}-ipython
 sudo port select --set gcc mp-gcc9
 sudo port select --set llvm mp-llvm-7.0
 sudo port select --set mysql mariadb-10.1
 sudo port select --set postgresql_select postgresql95
 sudo port select --set perl perl5.18-apple.15
-sudo port select --set nosetests nosetests37
+sudo port select --set nosetests nosetests${pythonver}
 sudo port select --set postgresql postgresql95
 sudo port select --set wxWidgets wxWidgets-3.0
 sudo port select --set maven maven3
-sudo port select --set sphinx py37-sphinx
-sudo port select --set pygments py37-pygments
-sudo port select --set virtualenv virtualenv37
+sudo port select --set sphinx py${pythonver}-sphinx
+sudo port select --set pygments py${pythonver}-pygments
+sudo port select --set virtualenv virtualenv${pythonver}
 
 port select --summary
 fi
