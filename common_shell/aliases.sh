eb6f0103c467ba5e29f9bf34cb735b52810a063a
Eitan Adler <lists@eitanadler.com>
2020-04-14 18:53:34 -0700

    aliases: fix dirsize with spaces

--- common_shell/aliases.sh
+++ common_shell/aliases.sh
@@ -101,7 +101,7 @@ mcd() {
 }
 
 dirsize() {
-	du -sk $* | sort -n | perl -ne 'if ( /^(\d+)\s+(.*$)/){$l=log($1+.1);$m=int($l/log(1024)); printf ("%6.1f\t%s\t%25s %s\n",($1/(2**(10*$m))),(("K","M","G","T","P")[$m]),"*"x (1.5*$l),$2);}';
+	du -sk "$@" | sort -n | perl -ne 'if ( /^(\d+)\s+(.*$)/){$l=log($1+.1);$m=int($l/log(1024)); printf ("%6.1f\t%s\t%25s %s\n",($1/(2**(10*$m))),(("K","M","G","T","P")[$m]),"*"x (1.5*$l),$2);}';
 }
 
 randpassword() {
