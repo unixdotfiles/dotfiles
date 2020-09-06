bda0b8cbe1ab48bc54875ea6423566259c311b3b
Eitan Adler <lists@eitanadler.com>
2020-09-06 11:13:22 -0700

    [shell] add TeX distribution

--- common_shell/env.sh
+++ common_shell/env.sh
@@ -17,7 +17,11 @@ _MYSQL_PATH=/opt/local/lib/mariadb-10.2/bin:/usr/local/mysql/bin
 _ANDROID_PATH=~/Library/Android/sdk/platform-tools
 _PERL_PATH=/opt/local/libexec/perl5.28/sitebin
 _LUA_PATH=~/.luarocks/bin
-export PATH="$HOME/bin:$_CCACHE_PATH:$_CARGO_PATH:$_HASKELL_PATH:$_NPM_PATH:$_GEM_PATH:$_USER_PIP_PATH:$_MACPORTS_PATH:$_PEAR_PATH:$_MYSQL_PATH:$_PERL_PATH:$_ANDROID_PATH:$_LUA_PATH:$_LOCAL_PATH:$PATH"
+if [ -d /usr/local/texlive/2020/bin/x86_64-darwin ]
+then
+  #_TEX_PATH=/usr/local/texlive/2020/bin/x86_64-darwin
+fi
+export PATH="$HOME/bin:$_CCACHE_PATH:$_CARGO_PATH:$_HASKELL_PATH:$_NPM_PATH:$_GEM_PATH:$_USER_PIP_PATH:$_MACPORTS_PATH:$_PEAR_PATH:$_MYSQL_PATH:$_PERL_PATH:$_ANDROID_PATH:$_LUA_PATH:$_TEX_PATH:$_LOCAL_PATH:$PATH"
 export CCACHE_DIR=/srv/obj/ccache
 export FORTUNE_PATH="/usr/share/games/fortune:/usr/local/share/games/fortune:$HOME/.fortune";
 
