e90bd170588c434feeaaeb8a6ae20ea1bfa5c3c6
Eitan Adler <lists@eitanadler.com>
2023-10-02 09:43:42 -0700

    [ps1] move @ sign to PS1_HOST so it can be hidden easily

--- zsh/ps1.zsh
+++ zsh/ps1.zsh
@@ -36,9 +36,9 @@ PS1_TIME="%F{3}%D{%H:%M:%S.%.} "
 PS1_USER="%F{blue}%n";
 if __inSSH
 then
-	PS1_HOST="%B%F{blue}%m%b"
+	PS1_HOST="@%B%F{blue}%m%b"
 else
-	PS1_HOST="%F{blue}%m";
+	PS1_HOST="@%F{blue}%m";
 fi
 PS1_ERR="%F{red}%(?.. !%?!)";    #return code of last command (if it was not 0)
 PS1_WD=" %F{magenta}%30<...<%~"; #current working directory limited to 30 chars
@@ -62,7 +62,7 @@ setCurrentPS1()
 	RPS1_PYTHON="${_PYTHON_ENV_NAME:+$_COLOR_PYTHON_ENV_NAME}"
 }
 
-PS1='[$PS1_HIST$PS1_TIME$PS1_USER@$PS1_HOST$PS1_BATTERY$PS1_EC2$PS1_WD$PS1_SHLVL$PS1_JOBS$PS1_ERR%f]$PS1_END'
+PS1='[$PS1_HIST$PS1_TIME${PS1_USER}${PS1_HOST}$PS1_BATTERY$PS1_EC2$PS1_WD$PS1_SHLVL$PS1_JOBS$PS1_ERR%f]$PS1_END'
 PS2='%F{cyan}%F{blue}(%F{green}%_%F{blue})%F{cyan}%f ';
 RPS1='${VIMRUNTIME:+"{$PS1_VIM}"}${RPS1_PYTHON}$PS1_VCS%f';
 
