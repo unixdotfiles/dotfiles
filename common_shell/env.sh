3c0e99d63db7bf93c0b856834359cb40d2b0366d
Eitan Adler <lists@eitanadler.com>
2024-01-14 23:57:51 -0800

    [zsh] fix include of _USER_PIP_PATH into PATH

--- common_shell/env.sh
+++ common_shell/env.sh
@@ -33,7 +33,7 @@ done
 # I need a better way to keep this up to date
 _TEX_PATH=/usr/local/texlive/2023/bin/universal-darwin
 
-export PATH="$_GO_PATH:$PATH:$HOME/bin:$_CCACHE_PATH:$_CARGO_PATH:$_HASKELL_PATH:$_NPM_PATH:$_GEM_PATH:$_MACPORTS_PATH:$_PEAR_PATH:$_MYSQL_PATH:$_PERL_PATH:$_ANDROID_PATH:$_LUA_PATH:$_TEX_PATH:$_VSCODE_PATH:$_PYENV_PATH:$_KITTEN_PATH:$_BREW_PATH:$_LOCAL_PATH"
+export PATH="$_GO_PATH:$PATH:$HOME/bin:$_CCACHE_PATH:$_CARGO_PATH:$_HASKELL_PATH:$_NPM_PATH:$_GEM_PATH:$_MACPORTS_PATH:$_PEAR_PATH:$_MYSQL_PATH:$_PERL_PATH:$_ANDROID_PATH:$_LUA_PATH:$_TEX_PATH:$_USER_PIP_PATH:$_VSCODE_PATH:$_PYENV_PATH:$_KITTEN_PATH:$_BREW_PATH:$_LOCAL_PATH"
 
 # This changes PATH amongst other things. It should be included above ideally.
 if [ -f /opt/homebrew/bin/brew ]
